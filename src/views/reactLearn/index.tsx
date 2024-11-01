63c1d4d4ac86c24586367d38c0d41937644f2e5a
homeScl <419863966@qq.com>
2024-11-01 11:06:08 +0800

    feat: 添加小卡片

--- src/views/reactLearn/index.tsx
+++ src/views/reactLearn/index.tsx
@@ -1,7 +1,10 @@
+import Card from "@/views/reactLearn/Card/index";
+
 const react1 = () => {
 	return (
 		<div>
 			<button onClick={() => window.onShow()}>全局弹框消息</button>
+			<Card />
 		</div>
 	);
 };
