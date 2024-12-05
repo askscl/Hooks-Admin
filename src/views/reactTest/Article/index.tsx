8001fb66e740e81a74d22fc8320ef1dd558c3869
homeScl <419863966@qq.com>
2024-12-05 11:17:59 +0800

    文章管理

--- src/views/reactTest/Article/index.tsx
+++ src/views/reactTest/Article/index.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom"
-import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from "antd"
+import { Link, useNavigate } from "react-router-dom"
+import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Popconfirm } from "antd"
 const { Option } = Select
 const { RangePicker } = DatePicker
 
@@ -11,11 +11,13 @@ import { useChannel } from "@/hooks/test/useChannel"
 import { useEffect, useState } from "react"
 
 const Article = () => {
+	const navigate = useNavigate()
 	// 获取频道列表
 	const { channelList } = useChannel()
 
 	// 状态枚举
 	const status = {
+		0: <Tag color="default">全部</Tag>,
 		1: <Tag color="warning">待审核</Tag>,
 		2: <Tag color="success">审核通过</Tag>
 	}
@@ -62,8 +64,15 @@ const Article = () => {
 			render: data => {
 				return (
 					<Space size="middle">
-						<Button type="primary" shape="circle" icon={<EditOutlined />} />
-						<Button type="primary" danger shape="circle" icon={<DeleteOutlined />} />
+						<Button
+							type="primary"
+							shape="circle"
+							icon={<EditOutlined />}
+							onClick={() => navigate("/reactTest/publish?id=" + data.id)}
+						/>
+						<Popconfirm title="确认删除文章吗?" onConfirm={() => confirmDelete(data.id)} okText="Yes" cancelText="No">
+							<Button type="primary" danger shape="circle" icon={<DeleteOutlined />} />
+						</Popconfirm>
 					</Space>
 				)
 			}
@@ -75,11 +84,12 @@ const Article = () => {
 		{
 			id: "82171",
 			comment_count: 0,
+			channel_id: 1,
 			cover: {
 				images: []
 			},
 			like_count: 0,
-			pubdate: "2019-03-11 09:00:00",
+			pubdate: "2024-12-01 09:00:00",
 			read_count: 2,
 			status: 1,
 			title: "wkwebview离线化加载h5资源解决方案"
@@ -87,11 +97,12 @@ const Article = () => {
 		{
 			id: "8212",
 			comment_count: 0,
+			channel_id: 2,
 			cover: {
 				images: []
 			},
 			like_count: 0,
-			pubdate: "2019-03-11 09:00:00",
+			pubdate: "2024-12-01 09:00:00",
 			read_count: 2,
 			status: 2,
 			title: "wkwebview离线化加载h5资源解决方案"
@@ -99,23 +110,162 @@ const Article = () => {
 		{
 			id: "8213",
 			comment_count: 0,
+			channel_id: 3,
 			cover: {
 				images: []
 			},
 			like_count: 0,
-			pubdate: "2019-03-11 09:00:00",
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 1,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8214",
+			comment_count: 0,
+			channel_id: 4,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 2,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8215",
+			comment_count: 0,
+			channel_id: 3,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 2,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8216",
+			comment_count: 0,
+			channel_id: 4,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 2,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8217",
+			comment_count: 0,
+			channel_id: 1,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 2,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8218",
+			comment_count: 0,
+			channel_id: 3,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 2,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8219",
+			comment_count: 0,
+			channel_id: 4,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
+			read_count: 2,
+			status: 2,
+			title: "wkwebview离线化加载h5资源解决方案"
+		},
+		{
+			id: "8220",
+			comment_count: 0,
+			channel_id: 5,
+			cover: {
+				images: []
+			},
+			like_count: 0,
+			pubdate: "2024-12-01 09:00:00",
 			read_count: 2,
 			status: 2,
 			title: "wkwebview离线化加载h5资源解决方案"
 		}
 	]
 
+	// 筛选功能
+	// 1.准备参数
+	const [reqData, setReqData] = useState({
+		status: 0,
+		channel_id: "",
+		begin_pubdate: "",
+		end_pubdate: "",
+		page: 1,
+		per_page: 4
+	})
+
+	// 获取文章列表
 	const [list, setList] = useState([])
 	const [count, setCount] = useState(0)
 	useEffect(() => {
-		setList(data)
-		setCount(data.length)
-	}, [])
+		const res = data.filter(item => {
+			if (reqData.status === 0) {
+				return item
+			} else {
+				return item.status === reqData.status
+			}
+		})
+		setList(res)
+		// setList(data)
+		setCount(res.length)
+	}, [reqData])
+
+	type FormValues = { status: any; channel_id: any; date: { format: (arg0: string) => any }[] }
+
+	// 获取筛选数据
+	const onFinish = (formValues: FormValues) => {
+		setReqData({
+			...reqData,
+			status: formValues.status,
+			channel_id: formValues.channel_id
+			// begin_pubdate: formValues.date[0]?.format("YYYY-MM-DD"),
+			// end_pubdate: formValues.date[1]?.format("YYYY-MM-DD")
+		})
+	}
+
+	// 删除
+	const confirmDelete = (id: string) => {
+		// 模拟接口
+		const res = list.filter(item => item.id !== id)
+		setList(res)
+		setCount(res.length)
+		// 触发重新请求表格数据
+		// setReqData({
+		// 	...reqData,
+		// 	page: 1
+		// })
+	}
+
 	return (
 		<div>
 			<Card
@@ -129,18 +279,18 @@ const Article = () => {
 				}
 				style={{ marginBottom: 20 }}
 			>
-				<Form initialValues={{ status: "" }}>
+				<Form initialValues={{ status: 0 }} onFinish={onFinish}>
 					<Form.Item label="状态" name="status">
 						<Radio.Group>
-							<Radio value={""}>全部</Radio>
-							<Radio value={0}>草稿</Radio>
+							<Radio value={0}>全部</Radio>
+							<Radio value={1}>草稿</Radio>
 							<Radio value={2}>审核通过</Radio>
 						</Radio.Group>
 					</Form.Item>
 
 					<Form.Item label="频道" name="channel_id">
 						<Select placeholder="请选择文章频道" style={{ width: 200 }}>
-							{channelList.map(item => (
+							{channelList.map((item: { id: number; name: string }) => (
 								<Option key={item.id} value={item.id}>
 									{item.name}
 								</Option>
@@ -161,7 +311,21 @@ const Article = () => {
 				</Form>
 			</Card>
 			<Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
-				<Table rowKey="id" columns={columns} dataSource={list} />
+				<Table
+					rowKey="id"
+					columns={columns}
+					dataSource={list}
+					pagination={{
+						total: count,
+						pageSize: reqData.per_page,
+						onChange: page => {
+							setReqData({
+								...reqData,
+								page
+							})
+						}
+					}}
+				/>
 			</Card>
 		</div>
 	)
--- src/views/reactTest/Publish/index.tsx
+++ src/views/reactTest/Publish/index.tsx
@@ -1,34 +1,160 @@
 import "./index.less"
 import { Card, Breadcrumb, Form, Button, Radio, Input, Upload, Space, Select, message } from "antd"
+import type { UploadFile } from "antd/es/upload/interface"
 import { PlusOutlined } from "@ant-design/icons"
-import { Link } from "react-router-dom"
-import React, { useState } from "react"
+import { Link, useSearchParams } from "react-router-dom"
+import React, { useEffect, useState } from "react"
 import { useChannel } from "@/hooks/test/useChannel"
+import axios from "axios"
 
 const { Option } = Select
+
+// 准备表格body数据
+const data = [
+	{
+		id: "82171",
+		comment_count: 0,
+		channel_id: 1,
+		content: "我只是个内容",
+		cover: {
+			type: 3,
+			images: [
+				"https://img1.baidu.com/it/u=498751481,2849377151&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=800",
+				"https://img2.baidu.com/it/u=576620719,3287590787&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800",
+				"https://img2.baidu.com/it/u=1341676861,1198740308&fm=253&fmt=auto&app=138&f=JPEG?w=475&h=475"
+			]
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 1,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8212",
+		comment_count: 0,
+		channel_id: 2,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8213",
+		comment_count: 0,
+		channel_id: 3,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 1,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8214",
+		comment_count: 0,
+		channel_id: 4,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8215",
+		comment_count: 0,
+		channel_id: 3,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8216",
+		comment_count: 0,
+		channel_id: 4,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8217",
+		comment_count: 0,
+		channel_id: 1,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8218",
+		comment_count: 0,
+		channel_id: 3,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8219",
+		comment_count: 0,
+		channel_id: 4,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	},
+	{
+		id: "8220",
+		comment_count: 0,
+		channel_id: 5,
+		cover: {
+			images: []
+		},
+		like_count: 0,
+		pubdate: "2024-12-01 09:00:00",
+		read_count: 2,
+		status: 2,
+		title: "wkwebview离线化加载h5资源解决方案"
+	}
+]
 
 const Publish: React.FC = () => {
 	// 获取频道列表
 	const { channelList } = useChannel()
 
-	// 提交表单
-	const onFinish = (values: any) => {
-		if (imageList.length !== imageType) return message.warn("封面图片数量不对！")
-		const { title, channel_id, content } = values
-		const reqData = {
-			title,
-			channel_id,
-			content,
-			cover: {
-				type: imageType,
-				images: imageList.map(item => item.response.data.url)
-			}
-		}
-		console.log(reqData)
-	}
-
 	// 上传图片
-	const [imageList, setImageList] = useState([])
+	const [imageList, setImageList] = useState<UploadFile[]>([])
 	const onUploadChange = (info: any) => {
 		console.log(info)
 		setImageList(info.fileList)
@@ -40,6 +166,81 @@ const Publish: React.FC = () => {
 		setImageType(e.target.value)
 	}
 
+	// 回填数据
+	const [searchParams] = useSearchParams()
+	const articleId = searchParams.get("id")
+	// console.log(articleId)
+	// 获取实例
+	const [form] = Form.useForm()
+
+	useEffect(() => {
+		// 1.通过id拿到数据
+		function getDetail() {
+			// 获取文章详情
+			const res = data.find((item: any) => item.id === articleId)
+
+			if (res) {
+				// console.log(res)
+				form.setFieldsValue({
+					...res,
+					type: res.cover.type
+				})
+				// 设置封面图片
+				if (res.cover.type !== undefined) {
+					setImageType(res.cover.type)
+				} else {
+					// 处理 undefined 的情况，例如设置一个默认值
+					setImageType(0) // 假设 0 是一个合适的默认值
+				}
+
+				setImageList(
+					res?.cover.images.map((url, index) => {
+						return {
+							url,
+							name: url,
+							uid: index.toString()
+						}
+					})
+				)
+			}
+		}
+		articleId && getDetail()
+		// 2.回填数据
+	}, [articleId, form])
+
+	// 提交表单
+	const onFinish = (formValues: any) => {
+		if (imageList.length !== imageType) return message.warn("封面图片数量不对！")
+		const { title, channel_id, content } = formValues
+		const reqData = {
+			title,
+			channel_id,
+			content,
+			cover: {
+				type: imageType,
+				images: imageList.map(item => {
+					if (item.response) {
+						return item.response.data.url
+					} else if (item.url) {
+						return item.url
+					}
+				})
+			}
+		}
+		console.log(reqData)
+		if (articleId) {
+			// 编辑文章
+			axios.put(`http://geek.itheima.net/v1_0/articles/${articleId}`, reqData).then(res => {
+				console.log(res)
+			})
+		} else {
+			// 发布文章
+			axios.post("http://geek.itheima.net/v1_0/articles", reqData).then(res => {
+				console.log(res)
+			})
+		}
+	}
+
 	return (
 		<div className="publish">
 			<Card
@@ -48,11 +249,17 @@ const Publish: React.FC = () => {
 						<Breadcrumb.Item>
 							<Link to={"/home/index"}>首页</Link>
 						</Breadcrumb.Item>
-						<Breadcrumb.Item>发布文章</Breadcrumb.Item>
+						<Breadcrumb.Item>{articleId ? "编辑文章" : "发布文章"}</Breadcrumb.Item>
 					</Breadcrumb>
 				}
 			>
-				<Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 0 }} onFinish={onFinish}>
+				<Form
+					form={form}
+					labelCol={{ span: 4 }}
+					wrapperCol={{ span: 16 }}
+					initialValues={{ type: 0 }}
+					onFinish={onFinish}
+				>
 					<Form.Item label="标题" name="title" rules={[{ required: true, message: "请输入文章标题" }]}>
 						<Input placeholder="请输入文章标题" style={{ width: 400 }} />
 					</Form.Item>
@@ -80,6 +287,7 @@ const Publish: React.FC = () => {
 								onChange={onUploadChange}
 								maxCount={imageType}
 								listType="picture-card"
+								fileList={imageList}
 								showUploadList
 							>
 								<div style={{ marginTop: 8 }}>
