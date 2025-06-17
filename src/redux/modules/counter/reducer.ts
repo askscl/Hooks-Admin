b9be0b07c69b7bedaac34df8eeefb8911fc81fd6
homeScl <419863966@qq.com>
2025-06-17 11:38:03 +0800

    恢复缩进

--- src/redux/modules/counter/reducer.ts
+++ src/redux/modules/counter/reducer.ts
@@ -4,24 +4,24 @@ import { CounterState } from "@/redux/interface"
 import * as types from "@/redux/mutation-types"
 
 const counterState: CounterState = {
-    count: 100
+	count: 100
 }
 
 const counter = (state: CounterState = counterState, action: AnyAction) =>
-    produce(state, draftState => {
-        switch (action.type) {
-            case types.SET_COUNT:
-                draftState.count = action.count
-                break
-            case types.INCREMENT:
-                draftState.count += 1
-                break
-            case types.DECREMENT:
-                draftState.count -= action.num
-                break
-            default:
-                return draftState
-        }
-    })
+	produce(state, draftState => {
+		switch (action.type) {
+			case types.SET_COUNT:
+				draftState.count = action.count
+				break
+			case types.INCREMENT:
+				draftState.count += 1
+				break
+			case types.DECREMENT:
+				draftState.count -= action.num
+				break
+			default:
+				return draftState
+		}
+	})
 
 export default counter
